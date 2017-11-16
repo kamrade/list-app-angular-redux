2b3d7274aaa9d39911ec31cd00f702351507ee74
Denis Michailov <kamrad.reg@gmail.com>
2017-11-16 22:45:04 +1000

    using immutable objects

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
 import { tassign } from 'tassign';
 import { INCREMENT } from './actions';
+import { Map } from 'immutable';
 
 export interface IAppState {
   counter: number;
@@ -15,12 +16,17 @@ export const INITIAL_STATE: IAppState = {
   }
 };
 
-export function rootReducer(state: IAppState, action): IAppState {
+export function rootReducer(state: Map<string, any>, action): Map<string, any> {
   switch (action.type) {
     case INCREMENT:
+      // to prevent mutation like this
+      // state.counter = state.counter++;
+      // good practice is to use unit-tests
+
       // return { counter: state.counter + 1 };
       // return Object.assign({}, state, { counter: state.counter + 1 });
-      return tassign(state, {counter: state.counter + 1 });
+      // return tassign(state, {counter: state.counter + 1 });
+      return state.set('counter', state.get('counter') + 1);
 
     default:
       return state;
