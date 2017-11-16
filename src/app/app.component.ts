6e689af6c6c713a066252906278669a0fb931cbf
Denis Michailov <kamrad.reg@gmail.com>
2017-11-16 22:02:18 +1000

    implement simple counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
+import { IAppState } from './store';
+import { INCREMENT } from './actions';
 
 @Component({
   selector: 'cp-root',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'cp';
+  title = 'Cardpay Dashboard';
+  counter = 0;
+
+  constructor(private ngRedux: NgRedux<IAppState>) {
+    ngRedux.subscribe(() => {
+      const store = ngRedux.getState();
+      this.counter = store.counter;
+    });
+  }
+
+  increment() {
+    this.ngRedux.dispatch({ type: INCREMENT });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgRedux, NgReduxModule } from 'ng2-redux';
 
 import { AppComponent } from './app.component';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 
 @NgModule({
@@ -10,9 +11,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  // configure initial store
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  }
+}
