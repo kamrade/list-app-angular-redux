356725268ab54d562baddf7fcb68f866486a22fd
Denis Michailov <kamrad.reg@gmail.com>
2017-11-17 22:29:13 +1000

    setup lists

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'cp-list-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-item.component.sass']
 })
 export class ListItemComponent implements OnInit {
+
+  @Input() listTitle;
 
   constructor() { }
 
