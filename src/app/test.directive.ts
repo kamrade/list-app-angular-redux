131c51917464d0a42160a9ee7ab6f5c5ab947679
Denis Michailov <kamrad.reg@gmail.com>
2017-11-22 18:06:48 +1000

    add dynamic click

--- src/app/test.directive.ts
+++ src/app/test.directive.ts
@@ -12,6 +12,8 @@ export class TestDirective {
     // console.dir(this.elementRef.nativeElement);
     const buttonElement = this.renderer.createElement('button');
     buttonElement.setAttribute('class', 'btn btn-sm btn-primary');
+    // this.renderer.listen()
+    this.renderer.listen(buttonElement, 'click', () => console.log('dynamic click'));
     const text = this.renderer.createText('text');
     this.renderer.appendChild(buttonElement, text);
     this.renderer.appendChild(this.elementRef.nativeElement, buttonElement);
