1c00c6715ad8891391cddef4da4f7191bcb60034
Denis Michailov <kamrad.reg@gmail.com>
2017-11-28 18:06:28 +1000

    restore backend

---
+++ list-app-backend/listModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// OPTIONS
+const titleMax = 240;
+
+const listSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    max: titleMax,
+    required: true
+  }
+});
+
+module.exports = listSchema;
---
+++ list-app-backend/server.js
@@ -0,0 +1,68 @@
+// VENDORS
+const express      = require('express');
+const mongoose     = require('mongoose');
+const bodyParser   = require('body-parser');
+const logger       = require('morgan');
+const errorHandler = require('errorhandler');
+const ok           = require('okay');
+
+// Access-Control-Allow-Origin
+const cors         = require('cors');
+
+const app          = express();
+const dbUri        = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/listApp';
+
+// INITIALS
+const dbConnection = mongoose.createConnection(dbUri);
+const Schema       = mongoose.Schema;
+
+// MODELS
+const listSchema   = require('./listModel');
+let List = dbConnection.model('List', listSchema, 'list');
+
+// MIDDLEWARE
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Access-Control-Allow-Origin middleware
+app.use(cors());
+
+
+// LIST ROUTES
+app.get('/', function(req, res) {
+  res.send('ok');
+});
+
+app.get('/v1/lists', function(req, res, next) {
+  List.find({}, ok(next, function(lists) {
+    
+    setTimeout(function() {
+      res.send(lists);
+    }, 1400);
+    
+  }));
+});
+
+app.post('/v1/lists', function(req, res, next) {
+  let list = new List(req.body);
+  list.validate(ok(next, function(error) {
+    list.save(ok(next, function(results) {
+      res.send(results);
+    }));
+  }));
+});
+
+app.delete('/v1/lists/:id', function(req, res, next) {
+  List.findOne({ _id: req.params.id }, ok(next, function (list) {
+    list.remove(ok(next, function(results) {
+      res.send(results);
+    }));
+  }));
+});
+
+// MIDDLEWARE
+app.use(errorHandler())
+
+// SERVER
+app.listen(1111, () => console.log('Server is running on localhost:1111'));
